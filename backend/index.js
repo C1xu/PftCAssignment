02c9dbf56e5618ba2536a84e880c9b9fe01075fa
C1xu <siyuan.xu@hotmail.com>
2022-03-25 13:07:07 +0100

    Editted backend index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,5 @@
 import Express from "express";
 import cors from "cors";
-import session from "express-session";
-import { CreateUser, GetUser, HashPassword, GOOGLE_APPLICATION_CREDITIALS } from "./db.js";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import https from "https";
@@ -37,7 +35,7 @@ const startServer = () => {
 const startServerEncrypted = async () => {
   const sm = new SecretManagerServiceClient({
     projectId: "pftcxu",
-    keyFilename: GOOGLE_APPLICATION_CREDITIALS,
+    keyFilename: "./key.json",
   });
 
   const [pub] = await sm.accessSecretVersion({
@@ -64,54 +62,54 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/login.html"));
-});
+// app.get("/login", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/login.html"));
+// });
 
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/register.html"));
-});
+// app.get("/register", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/register.html"));
+// });
 
-app.post("/login", (req, res) => {
-  const email = req.query.email;
-  const password = req.query.password;
-  requests++;
-  GetUser(email).then((r) =>{
-    console.log(r);
-    if(r.length === 1){
-      if(r[0].password === HashPassword(password)){
-        console.log("Login Pass")
-        res.send({ result: "success", email: email, name: r[0].name});
-      }else{
-        console.log("Login Fail")
-        res.send({ result: "fail", reason: "account password does not match"});
-      }  
-    }else{
-      console.log("Account does not exists")
-      res.send({ result: "fail", reason: "account does not exists"});
-    }
-  });
-});
+// app.post("/login", (req, res) => {
+//   const email = req.query.email;
+//   const password = req.query.password;
+//   requests++;
+//   GetUser(email).then((r) =>{
+//     console.log(r);
+//     if(r.length === 1){
+//       if(r[0].password === HashPassword(password)){
+//         console.log("Login Pass")
+//         res.send({ result: "success", email: email, name: r[0].name});
+//       }else{
+//         console.log("Login Fail")
+//         res.send({ result: "fail", reason: "account password does not match"});
+//       }  
+//     }else{
+//       console.log("Account does not exists")
+//       res.send({ result: "fail", reason: "account does not exists"});
+//     }
+//   });
+// });
 
-app.post("/register", (req, res) => {
-  const name = req.query.name;
-  const surname = req.query.surname;
-  const email = req.query.email;
-  const password = req.query.password;
-  requests++;
+// app.post("/register", (req, res) => {
+//   const name = req.query.name;
+//   const surname = req.query.surname;
+//   const email = req.query.email;
+//   const password = req.query.password;
+//   requests++;
 
-  GetUser(email).then((r) =>{
-    if(r.length === 0){
-      CreateUser(name, surname, email, password).then((r) => {
-        //console.log(r);
-        res.send({ result: "success", name: name, surname: surname, email: email});
-      }).catch((e) => console.log(e));      
-    }else{
-      //console.log("Account already exists")
-      res.send({ result: "fail", reason: "account already exists"});
-    }
-  });  
-});
+//   GetUser(email).then((r) =>{
+//     if(r.length === 0){
+//       CreateUser(name, surname, email, password).then((r) => {
+//         //console.log(r);
+//         res.send({ result: "success", name: name, surname: surname, email: email});
+//       }).catch((e) => console.log(e));      
+//     }else{
+//       //console.log("Account already exists")
+//       res.send({ result: "fail", reason: "account already exists"});
+//     }
+//   });  
+// });
 
-//startServer();
+startServer();
 startServerEncrypted();
