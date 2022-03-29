7cfb6901863f6e1dbd983b1aa2a7c3e41af245b1
C1xu <siyuan.xu@hotmail.com>
2022-03-29 13:57:17 +0200

    Edit backend index.js

--- backend/index.js
+++ backend/index.js
@@ -64,58 +64,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
-app.use("auth", auth)
+app.use("/auth", auth)
 
-app.use("upload", upload)
+app.use("/upload", upload)
 
-// app.get("/login", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/login.html"));
-// });
-
-// app.get("/register", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/register.html"));
-// });
-
-// app.post("/login", (req, res) => {
-//   const email = req.query.email;
-//   const password = req.query.password;
-//   requests++;
-//   GetUser(email).then((r) =>{
-//     console.log(r);
-//     if(r.length === 1){
-//       if(r[0].password === HashPassword(password)){
-//         console.log("Login Pass")
-//         res.send({ result: "success", email: email, name: r[0].name});
-//       }else{
-//         console.log("Login Fail")
-//         res.send({ result: "fail", reason: "account password does not match"});
-//       }  
-//     }else{
-//       console.log("Account does not exists")
-//       res.send({ result: "fail", reason: "account does not exists"});
-//     }
-//   });
-// });
-
-// app.post("/register", (req, res) => {
-//   const name = req.query.name;
-//   const surname = req.query.surname;
-//   const email = req.query.email;
-//   const password = req.query.password;
-//   requests++;
-
-//   GetUser(email).then((r) =>{
-//     if(r.length === 0){
-//       CreateUser(name, surname, email, password).then((r) => {
-//         //console.log(r);
-//         res.send({ result: "success", name: name, surname: surname, email: email});
-//       }).catch((e) => console.log(e));      
-//     }else{
-//       //console.log("Account already exists")
-//       res.send({ result: "fail", reason: "account already exists"});
-//     }
-//   });  
-// });
-
-startServer();
+//startServer();
 startServerEncrypted();
