f5b0ce2cf21839744ac10c9be207a59638735de2
C1xu <siyuan.xu@hotmail.com>
2022-04-01 10:22:21 +0200

    Added Multer and updated upload.js

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,23 +1,59 @@
 import Express from "express";
+import multer from "multer";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const upload = Express.Router();
 
-upload.route("/").post((req, res) => {
-
-    console.log("Upload button pressed");
-    let base64String = "";
-    var reader = new FileReader();
-
-    reader.onload = function () {
-        base64String = reader.result.replace("data:", "")
-            .replace(/^.+,/, "");
-  
-        imageBase64Stringsep = base64String;
-  
-        console.log(base64String);
+let imageUpload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, "../uploads/"));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
+      return callback(new Error("Only images are allowed"));
     }
-    reader.readAsDataURL(req.query.formData);
+    callback(null, true);
+  },
+  limits: {
+    fileSize: 2621441,
+  },
+});
 
+upload.route("/").post(imageUpload.single("image"), (req, res) => {
+    if(req.file){
+        console.log("File downloaded at: " + req.file.path);
+    
+        // let base64String = "";
+        // var reader = new FileReader();
+
+        // reader.onload = function () {
+        //     base64String = reader.result.replace("data:", "")
+        //         .replace(/^.+,/, "");
+    
+        //     imageBase64Stringsep = base64String;
+    
+        //     console.log(base64String);
+        // }
+        // reader.readAsDataURL(req.query.formData);
+        
+        let bufferObj = Buffer.from(req, "utf8");
+        let base64String = bufferObj.toString("base64");
+        
+        res.send({
+            status: "200",
+            message: "File uploaded successfully! Processing..",
+        });
+    }
     //receive the image  Done?
     //convert it to base64  Done?
     //save it in cloud storage
