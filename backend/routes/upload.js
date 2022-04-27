fa9c390e494b8cb3c4a4435cfbaf2323899e9227
C1xu <siyuan.xu@hotmail.com>
2022-04-27 13:40:52 +0200

    Upload to bucket

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -33,7 +33,7 @@ let imageUpload = multer({
 
 async function uploadToBucket(file) {
     await storage.bucket("pftcxu.appspot.com").upload(file , {
-        destination: "pending/" + file.originalname,
+        destination: "pending/" + file.path.originalname,
     });
 }
 
