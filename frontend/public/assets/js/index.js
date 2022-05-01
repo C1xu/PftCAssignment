4423b4e4961fb243ec6ff91a9c5b0fa8d9d713dc
C1xu <siyuan.xu@hotmail.com>
2022-05-01 17:05:45 +0200

    Try admin and credit check

--- frontend/public/assets/js/index.js
+++ frontend/public/assets/js/index.js
@@ -1,5 +1,3 @@
-import { GetUser } from "../../../../backend/db";
-
 let signInButton = document.getElementById("signIn");
 let signOutButton = document.getElementById("signOut");
 let profile = document.getElementById("profile");
